45d9f581a22c1a1ca5ebca5fe85a22b62b602d7a
Jovanni_Ramirez <jovanni_r15@yahoo.com>
2024-10-29 13:33:08 -0700

    Add project files.

---
+++ JoeRamTestSite/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace JoeRamTestSite.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
